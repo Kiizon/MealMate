438fd6b48d8e99377c7d3d5d2c38b6889ba70ec1
Kiizon <kishddizon@gmail.com>
2025-12-30 08:25:26 -0500

    fix typo

--- api/main.py
+++ api/main.py
@@ -46,5 +46,5 @@ def get_deals(postal_code: str):
     
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.eviron.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host = "0.0.0.0", port = port, reload = True)
