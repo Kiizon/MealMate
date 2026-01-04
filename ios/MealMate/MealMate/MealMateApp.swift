bf9883d0b7cbc564c1d78ef856fcb7bd6135d353
Kiizon <kishddizon@gmail.com>
2026-01-04 04:01:38 -0500

    add ios project scaffold with swiftui

---
+++ ios/MealMate/MealMate/MealMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMateApp.swift
+//  MealMate
+//
+//  Created by Kish Dizon on 2025-12-30.
+//
+
+import SwiftUI
+
+@main
+struct MealMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
